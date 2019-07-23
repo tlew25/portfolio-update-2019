cc0a4e53cd7c4b1cd60ced18bde8a54c36d36f9f
Taylor Lewis <tlew@Taylors-MBP.attlocal.net>
2019-07-22 19:10:08 -0500

    new stuff

--- assets/js/website.js
+++ assets/js/website.js
@@ -1,6 +1,15 @@
 // MONTH & TIME
+var y = new Date();
+var d = y;
+var m = d;
 
-document.getElementById("time").innerHTML = new Date().toLocaleString();
+var months = 
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+document.getElementById("time").innerHTML = 
+months[m.getMonth()] + '/' + d.getDate() + '/' + y.getFullYear();
+
+///////////////////////////////////////////////////////////////////////////
 
 //FIREBASE JS FOR CLIENT STORAGE
 var config = {
@@ -58,3 +67,65 @@ function saveMessage(name, email, phone, comments) {
     comments: comments
   });
 }
+///////////////////////////////////////////////////////////////////////////
+
+// BACKGROUND JS TRANSTIONS
+
+// document.getElementById('img-main').addEventListener('click', changeColor);
+
+// function changeColor(){
+// document.body.style.backgroundImage = 'linear-gradient(to left, green , yellow)';
+//  return changeColor();
+// };
+
+
+// function welcome(){
+//   // Show alert
+//    var w = document.querySelector(".jumbotron");
+
+//   // Hide alert after 3 seconds
+//   setTimeout(function() {
+//     w.style.display = "block";
+//     w.style.opacity = "0";
+//   }, 2000);
+// };
+// welcome();
+
+
+
+// var years = [12, 35, 85, 24, 27];
+
+// function calcArr(arr, fn){
+
+// var arrHolder = [];
+
+// for(var i = 0; i < arr.length; i++){
+
+//   arrHolder.push(fn(arr[i]));
+//   }
+//  return arrHolder;
+// };
+
+// function calcAge(el){
+// const x = 2019 - el;
+// return document.getElementById('agePrint').innerHTML = x;
+
+// };
+
+
+
+// var fullAge = calcArr(years, calcAge);
+
+// console.log(fullAge);
+
+
+// function math(sub, div){
+// let numArrOne = [15 + 30 + 78 + 25];
+// let numArrTwo = [55 + 78 + 90 + 100];
+
+// const sum = numArrOne * numArrTwo / div - sub;
+
+// return document.getElementById('sum').innerHTML = sum;
+// };
+// math(350, 3);
+
